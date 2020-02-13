4d2ec31931bb21f98bdd001119cf0be78307b935
Eliseo Abel <eliseoabelcarh@gmail.com>
2020-02-13 02:37:47 -0300

    .

--- TrabajoFinal/dashboard.js
+++ TrabajoFinal/dashboard.js
@@ -22,6 +22,7 @@ botonVerificar.addEventListener("click",mostrarAreaVideo )
 var contenedorVideo = document.getElementById("contenedor-Video")
 
 
+
 window.onload = function() {
     
     cargarMontoCambio()
@@ -31,6 +32,30 @@ window.onload = function() {
 
 
 
+  // jquery código 
+
+var input1 = document.getElementById("codigocomprobante")
+var input2 = document.getElementById("cajitacheck")
+
+var mensajeExitoso= "Bien! es todo por ahora"
+var mensajeError = "Hay errores en formulario"
+
+
+
+
+$("#cerrarPopup").click(cerrarPopup)
+$("#popup").click(cerrarPopup)
+
+$("#botonEnviar").click(function(){
+  var input1esValido = input1.validity.valid
+  var input2esValido = input2.validity.valid
+
+  if( !input1esValido || !input2esValido ) {
+    $("#contenidoPopup").html(mensajeError)
+    mostrarPopup()
+    
+  }
+})
 
   
 
